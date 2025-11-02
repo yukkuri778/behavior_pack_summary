61a19477d8c1416363fa55192a011e8c61e0f1d7
yukkuri778 <yukkuri77789@gmail.com>
2025-11-02 20:41:47 +0900

    マイルストーン機能修正

--- scripts/main.js
+++ scripts/main.js
@@ -113,15 +113,22 @@ world.afterEvents.playerBreakBlock.subscribe(event => {
         const newScore = objective.addScore(player, 1);
 
         // お祝いの閾値リスト
-        const CELEBRATION_MILESTONES = [100, 1000, 2000, 3000, 5000, 10000, 20000, 30000, 40000, 50000, 100000, 200000, 300000, 500000, 1000000];
+        const CELEBRATION_MILESTONES = [100, 1000, 2000, 3000, 5000, 10000, 20000, 30000, 40000, 50000];
 
         // スコアがリストに含まれているかチェック
         if (CELEBRATION_MILESTONES.includes(newScore)) {
             // お祝いメッセージ
-            world.sendMessage(`§l§k!!!§r §b${player.name}§r が採掘数 §e${newScore}個§r を達成しました！ §k!!!§r`);
+            world.sendMessage(`§l§k!!!§r §b${player.name}§r が採掘数 §e${newScore}個§r を突破しました！ §k!!!§r`);
             // 花火を打ち上げる
             player.dimension.spawnEntity("minecraft:fireworks_rocket", player.location);
             // 経験値取得音を全プレイヤーに再生
+            world.playSound("random.pop", player.location, { volume: 0.8, pitch: 1.0, players: world.getAllPlayers() });
+        }
+
+        //以降１０万ごとにお祝い
+        else if(newScore % 100000 == 0){ 
+            world.sendMessage(`§l§k!!!§r §b${player.name}§r が採掘数 §e${newScore}個§r を突破しました！ §k!!!§r`);
+            player.dimension.spawnEntity("minecraft:fireworks_rocket", player.location);
             world.playSound("random.pop", player.location, { volume: 0.8, pitch: 1.0, players: world.getAllPlayers() });
         }
     } catch (e) {
@@ -133,13 +140,13 @@ world.afterEvents.playerBreakBlock.subscribe(event => {
         const objective = world.scoreboard.getObjective(WORLD_MINING_COUNT_OBJECTIVE);
         const newWorldScore = objective.addScore(WORLD_TOTAL_HOLDER, 1);
 
-        // お祝いの閾値リスト（プレイヤーと共通）
-        const CELEBRATION_MILESTONES = [1000, 3000, 5000, 10000, 30000, 50000, 100000, 200000, 300000, 400000, 500000, 1000000];
+        // お祝いの閾値リスト
+        const CELEBRATION_MILESTONES = [1000, 3000, 5000, 10000, 30000, 50000, 100000, 200000, 300000, 400000, 500000];
 
         // ワールドの総採掘数がリストに含まれているかチェック
         if (CELEBRATION_MILESTONES.includes(newWorldScore)) {
             // お祝いメッセージ
-            world.sendMessage(`§l§k!!!§r §dワールド総採掘数§r が §e${newWorldScore}個§r を達成しました！ §k!!!§r`);
+            world.sendMessage(`§l§k!!!§r §dワールド総採掘数§r が §e${newWorldScore}個§r に到達しました！ §k!!!§r`);
             // 全てのプレイヤーから花火を打ち上げる
             for (const p of world.getAllPlayers()) {
                 p.dimension.spawnEntity("minecraft:fireworks_rocket", p.location);
@@ -147,6 +154,16 @@ world.afterEvents.playerBreakBlock.subscribe(event => {
             // 経験値取得音を全プレイヤーに再生
             world.playSound("random.levelup", player.location, { volume: 1.0, pitch: 0.8, players: world.getAllPlayers() });
         }
+
+        //以降１００万ごとにお祝い
+        else if(newWorldScore % 1000000 == 0){
+            world.sendMessage(`§l§k!!!§r §dワールド総採掘数§r が §e${newWorldScore}個§r に到達しました！ §k!!!§r`);
+            for (const p of world.getAllPlayers()) {
+                p.dimension.spawnEntity("minecraft:fireworks_rocket", p.location);
+            }
+            world.playSound("random.levelup", player.location, { volume: 1.0, pitch: 0.8, players: world.getAllPlayers() });
+        }
+
     } catch (e) {
         console.error(`[MiningRanking] Failed to add score to world total: ${e}`);
     }
